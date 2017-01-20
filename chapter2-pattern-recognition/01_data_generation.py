0cfbfe5c6fd1073e29c1649aa72475a8d3c77288
ybbarng <ybbarng@gmail.com>
2017-01-20 17:31:12 +0900

    평균값을 표시하게 함

--- chapter2-pattern-recognition/01_data_generation.py
+++ chapter2-pattern-recognition/01_data_generation.py
@@ -26,6 +26,15 @@ X1 = np.random.randn(N, 2).dot(sqrtm(s1)) + i1
 X2 = np.random.randn(N, 2).dot(sqrtm(s2)) + i2
 
 # Plot the dataset
+xytext = (20, 20)
+textcoords = 'offset points'
+bbox = dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5)
+arrowprops = dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+
+pl.plot(m1[0], m1[1], 'o')
+pl.annotate('${\mu}_1$', xy=m1, xytext=xytext, textcoords=textcoords, bbox=bbox, arrowprops=arrowprops)
+pl.plot(m2[0], m2[1], 'o')
+pl.annotate('${\mu}_2$', xy=m2, xytext=xytext, textcoords=textcoords, bbox=bbox, arrowprops=arrowprops)
 pl.plot(X1[:,0],X1[:,1], '+')
 pl.plot(X2[:,0],X2[:,1], 'd')
 pl.grid('on')
